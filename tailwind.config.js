40140129fbd6bd6d769ddab5ceee613e8e8a0e4c
Dávid Tkáč <david@neoloop.cz>
2023-11-23 21:06:59 +0100

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
-/ @type {import('tailwindcss').Config} */
+/ @type {import('tailwindcss').Config} */;
 module.exports = {
   content: ["./src//*.{html,js}"],
   theme: {
-    extend: {},
+    container: { center: true },
+    extend: {
+      colors: { primary: "#EF4130" },
+      boxShadow: { primary: "0px 5px 30px 0px rgba(239, 65, 48, 0.80)" },
+      backgroundImage: { heroImage: "linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 35%), url('../src/bgimage.png')" },
+      fontFamily: {
+        tabac: [
+          "TabacBigSans-Regular",
+          "TabacBigSans-Bold",
+          "TabacBigSans-SemiBold",
+        ],
+
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
